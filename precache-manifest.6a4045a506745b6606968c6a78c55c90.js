f8f3b68bdd09e87024c9b91bb974a170a7777348
Mary-Tyler-Moore <46167401+Mary-Tyler-Moore@users.noreply.github.com>
2019-05-14 08:23:40 -0400

    Add files via upload

---
+++ precache-manifest.6a4045a506745b6606968c6a78c55c90.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "b79f5c630b31c8fccfb5d9e8c8287ddd",
+    "url": "assets/img/tile.png"
+  },
+  {
+    "revision": "f003c709fa0a1531a289",
+    "url": "game.0f6c489874e7912154db.js"
+  },
+  {
+    "revision": "2d01343132939c818c8d53af2460cb84",
+    "url": "index.html"
+  },
+  {
+    "revision": "fce01705782b2b0cfd332771d6c1fabf",
+    "url": "sw.js"
+  },
+  {
+    "revision": "895a111464ab166c4a17",
+    "url": "vendors.78b7c76ef17afd3077ee.js"
+  },
+  {
+    "revision": "735b5a83cfd0dfd278dcbf364c462c0f",
+    "url": "assets/img/wholes-small.png"
+  },
+  {
+    "revision": "17a48be59e9cf44e29c0aee359fb9349",
+    "url": "assets/textureAtlas.png"
+  },
+  {
+    "revision": "b14fdd36c61d7a9c32c0beaed4177bcc",
+    "url": "assets/textureAtlas.tps"
+  },
+  {
+    "revision": "f9ea4ce1455483e71ece5fdf2172e1f7",
+    "url": "assets/img/restart-btn.png"
+  },
+  {
+    "revision": "79573c486e22b34de1a769a7e5076d2f",
+    "url": "assets/img/grass.png"
+  },
+  {
+    "revision": "abdf954b5a1bf7a891f4bafc3797d7f7",
+    "url": "assets/textureAtlas.json"
+  },
+  {
+    "revision": "ed49048255da4bdaadf5b03991e204ad",
+    "url": "assets/img/sky.png"
+  },
+  {
+    "revision": "2abdc6aa49e910d8676d90113ae91c73",
+    "url": "assets/terrain.svg"
+  },
+  {
+    "revision": "87700e15b50fc50161127b5aadbfff30",
+    "url": "assets/img/gas.png"
+  },
+  {
+    "revision": "05ff330f45121732338a695b67134903",
+    "url": "assets/img/car-wheel.png"
+  },
+  {
+    "revision": "95add5ae695303ca3fcb52a5b1aba60d",
+    "url": "assets/img/car-body.png"
+  },
+  {
+    "revision": "8e8fe5793eebae1ea472e35efb982b58",
+    "url": "assets/icons/icons-512.png"
+  },
+  {
+    "revision": "af5c1203bb508a8ca58ad6564b62a46d",
+    "url": "assets/icons/icons-192.png"
+  },
+  {
+    "revision": "57040e5677322118f6d56a1d9e43c5c6",
+    "url": "assets/icons/favicon.ico"
+  }
+];
---
+++ sw.js
@@ -0,0 +1,4 @@
+importScripts("precache-manifest.6a4045a506745b6606968c6a78c55c90.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+workbox.precaching.precacheAndRoute(__precacheManifest)
+
